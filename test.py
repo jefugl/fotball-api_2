2cb0c47b789e6ab949cbfd8f8852810aed4b16ed
jefugl <107198804+jefugl@users.noreply.github.com>
2025-04-04 19:24:58 +0200

    table update

--- test.py
+++ test.py
@@ -1,22 +1,35 @@
-from oob_spam import PremierLeague
+# from oob_spam import PremierLeague
+#
+#
+# def fixture(url):
+#     pl_fix = PremierLeague(url)
+#     response = pl_fix.pl_api()
+#     print(response.text)
+#
+#
+# def table(url):
+#     pl_table = PremierLeague(url)
+#     response = pl_table.pl_api()
+#     print(response.text)
+#
+#
+# link = ' https://v3.football.api-sports.io/fixtures?league=39&season=2024'
+# link2 = 'https://v3.football.api-sports.io/standings?league=39&season=2024'
+#
+#
+# if __name__ == '__main__':
+#     fixture(link)
+#     table(link2)
 
 
-def fixture(url):
-    pl_fix = PremierLeague(url)
-    response = pl_fix.pl_api()
-    print(response.text)
+import json
+from datetime import datetime
 
 
-def table(url):
-    pl_table = PremierLeague(url)
-    response = pl_table.pl_api()
-    print(response.text)
+with open('./matches/match.json') as f:
+    data = json.load(f)
 
-
-link = ' https://v3.football.api-sports.io/fixtures?league=39&season=2024'
-link2 = 'https://v3.football.api-sports.io/standings?league=39&season=2024'
-
-
-if __name__ == '__main__':
-    fixture(link)
-    table(link2)
+for match in data['response']:
+    timestamp = match['fixture']['timestamp']
+    date = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d") # Sjekk formatet her.
+    print(date)
